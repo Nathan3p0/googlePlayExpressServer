643cf7fae5929a838ba88ac4f9f40fde9d622632
Nathan Szelag <nszelag@live.com>
2019-07-30 22:01:58 -0400

    added mocha/chai and supertest unit and integration tests

---
+++ test/app.test.js
@@ -0,0 +1,41 @@
+const expect = require('chai').expect
+const request = require('supertest')
+const app = require('../index.js')
+
+describe('get playstore', () => {
+    it('returns array of apps', () => {
+        return request(app)
+            .get('/apps')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body)
+                    .to.be.an('array')
+            })
+    })
+    it('Status 400 on invalid sort value', () => {
+        return request(app)
+            .get('/apps')
+            .query({ sort: 'butts' })
+            .expect(400, 'Please enter a sort value of either Rating or App')
+    })
+    it('Status 200 on valid sort value', () => {
+        return request(app)
+            .get('/apps')
+            .query({ sort: 'Rating' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(
+                res => {
+                    expect(res.body).to.be.an('array');
+                    let app = 0;
+                    let sorted = true;
+                    while (sorted && app < res.body.length - 1) {
+                        sorted = sorted && res.body[app].Rating <= res.body[app + 1].Rating;
+                        app++;
+                    }
+                    expect(sorted).to.be.true;
+                }
+            )
+    })
+})
